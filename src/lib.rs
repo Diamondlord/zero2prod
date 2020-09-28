269747655cd42116117f25742de1a543019e3948
Diamondlord <vladimir.almazov@gmail.com>
2020-09-28 23:25:35 +0300

    add subscription integration tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,22 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+// Let's start simple: we always return a 200 OK
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     // No .await here!
     Ok(server)
 }
