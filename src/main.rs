1361ed4763467a089d4725182abc11e24757593d
Diamondlord <vladimir.almazov@gmail.com>
2020-10-10 00:07:40 +0300

    add config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port 8080");
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
