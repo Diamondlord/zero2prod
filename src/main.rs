960e8b2315cfa25f7b0a4c6fd4ab5cb56fe9ee6e
Diamondlord <vladimir.almazov@gmail.com>
2020-11-07 20:48:43 +0200

    less connect time to db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -8,11 +8,15 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_string = configuration.database.connection_string();
-    let pg_pool = PgPool::connect(&connection_string)
+
+    let pg_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
