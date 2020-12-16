c48c33f8fb741386f2565b532875b460451442be
Diamondlord <vladimir.almazov@gmail.com>
2020-12-16 19:13:15 +0200

    type driven development -> new type SubscriberName

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct SubscribeRequest {
@@ -19,33 +20,39 @@ or we'd like to specify more explicitly what should/how they should be captured
 (e.g. naming each field of payload) - we can explicitly tell tracing to ignore them using the skip directive.
 */
 #[tracing::instrument(
-    name = "Adding a new subscriber",
-    skip(payload, db_pool),
-    fields(
-        request_id=%Uuid::new_v4(),
-        email = %payload.email,
-        name = %payload.name
-    )
+name = "Adding a new subscriber",
+skip(form, db_pool),
+fields(
+request_id = % Uuid::new_v4(),
+email = % form.email,
+name = % form.name
+)
 )]
 pub async fn subscribe(
-    payload: web::Form<SubscribeRequest>,
+    form: web::Form<SubscribeRequest>,
     // Retrieving a connection from the application state!
     db_pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    insert_subscriber(db_pool, payload).await.map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        HttpResponse::InternalServerError().finish()
-    })?;
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name),
+    };
+    insert_subscriber(db_pool, &new_subscriber)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        })?;
     Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(
-    name = "Saving new subscriber details in the database",
-    skip(payload, db_pool)
+name = "Saving new subscriber details in the database",
+skip(new_subscriber, db_pool)
 )]
 async fn insert_subscriber(
     db_pool: web::Data<PgPool>,
-    payload: web::Form<SubscribeRequest>,
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     let query = sqlx::query!(
         r#"
@@ -53,8 +60,8 @@ async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        payload.email,
-        payload.name,
+        new_subscriber.email,
+        new_subscriber.name.inner_ref(),
         Utc::now()
     );
     query.execute(db_pool.as_ref()).await.map_err(|e| e)?;
