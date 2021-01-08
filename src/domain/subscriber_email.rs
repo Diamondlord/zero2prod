2a903b2d277c4530b75d6f7dd69cafc4c3798981
Diamondlord <vladimir.almazov@gmail.com>
2021-01-08 20:17:32 +0200

    implemen property based testing email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -24,6 +24,7 @@ mod tests {
     use super::SubscriberEmail;
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use quickcheck::{Arbitrary, Gen};
     use claim::assert_err;
 
     #[test]
@@ -50,9 +51,19 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        assert_ok!(SubscriberEmail::parse(email));
+    // Both `Clone` and `Debug` are required by `quickcheck`
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
